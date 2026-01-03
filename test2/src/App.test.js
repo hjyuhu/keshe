4a1078d40eacfe95620344510a2657db7367c4f8
hjyuhu <1957977900@qq.com>
2026-01-03 22:25:19 +0800

    覆盖旧版本

--- test2/src/App.test.js
+++ test2/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/舆情情感实时监测系统/i);
+  expect(titleElement).toBeInTheDocument();
 });
